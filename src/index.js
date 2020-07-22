d34015c23675f8402519234d42a7a0023a47db33
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-07-22 22:12:55 +0300

    feat: burger menu + its' adaptive

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 import phoneAccordeon from './modules/phoneAccordeon';
+import burgerMenu from './modules/burgerMenu';
 
 phoneAccordeon();
+burgerMenu();
---
+++ src/modules/burgerMenu.js
@@ -0,0 +1,37 @@
+const burgerMenu = () => {
+  const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+
+  const setStartingPosition = (elem) => {
+    if (document.documentElement.clientWidth < 576 && elem.style.right != 0) {
+      elem.style.transition = '0';
+      elem.style.cssText = `top: -${elem.clientHeight}px; right: ${elem.clientWidth}px`;
+      elem.style.transition = '1s';
+    } else {
+      elem.style.cssText = '';
+    }
+  };
+
+  setStartingPosition(popupDialogMenu);
+
+  window.addEventListener("resize", () => {
+    setStartingPosition(popupDialogMenu);
+  });
+
+  document.addEventListener('click', (event) => {
+    let target = event.target;
+    
+
+    if (target.closest('.menu')) {
+      popupDialogMenu.style.cssText = `top: 0px; 
+                                       right: ${popupDialogMenu.clientWidth}px;
+                                       ${document.documentElement.clientWidth < 576 ? "transform: translate3d(0,0,0)" : ""}`;
+    } else {
+      if (!target.closest('.popup-dialog-menu') || target.closest('.popup-menu-nav') || target.closest('.close-menu')) {
+        setStartingPosition(popupDialogMenu);
+      }
+    }
+
+  });
+};
+
+export default burgerMenu;
