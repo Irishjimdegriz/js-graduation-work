9e005a0ed69fa8bdd27bd64c9af7c3c74beb9b36
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-07-22 23:22:49 +0300

    feat: phone mask

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import phoneAccordeon from './modules/phoneAccordeon';
 import burgerMenu from './modules/burgerMenu';
 import smoothScrolling from './modules/smoothScrolling';
 import fullList from './modules/fullList';
+import phoneMask from './modules/phoneMask';
 
 phoneAccordeon();
 burgerMenu();
 smoothScrolling();
 fullList();
+phoneMask();
