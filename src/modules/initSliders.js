2d71be8df5b079a416a9ef367677cac9339b4c32
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-07-24 20:37:28 +0300

    feat: reviews slider

--- src/modules/initSliders.js
+++ src/modules/initSliders.js
@@ -1,21 +1,24 @@
 class Slider{
-  constructor({main, wrap, position = 0, next, prev, slidesToShow = 1, infinity = true, activeItemClass = ''}){
+  constructor({main, wrap, position = 0, next, prev, slideSelector, slidesToShow = 1, infinity = true, activeItemClass = '', reviewsSlider = false}){
       this.main = document.querySelector(main);
+      this.mainSelector = main;
       this.wrap = document.querySelector(wrap);
+      this.wrapSelector = wrap;
       this.slides = document.querySelector(wrap).children;
+      this.slideSelector = slideSelector;
       this.next = document.querySelector(next);
       this.prev = document.querySelector(prev);
       this.slidesToShow = slidesToShow;
+      this.reviewsSlider = reviewsSlider;
       this.options = {
           position,
           infinity,
           slideWidth: Math.floor(100 / this.slidesToShow),
           activeItemClass
       };
-
   }
 
-  init(){
+  init() {
       this.addGloClass();
       this.addSlyles();
       this.highlightActiveItem();
@@ -25,26 +28,27 @@ class Slider{
       }
       
       this.controlSlider();
-  }
 
-  addGloClass(){
-      this.main.classList.add('glo-slider');
-      this.wrap.classList.add('glo-slider__wrap');
-
-      for(let item of this.slides) {
-        item.classList.add('glo-slider__item');
+      if (!this.options.infinity) {
+        this.prev.style.visibility= 'hidden';
       }
   }
 
-  addSlyles(){
+  addGloClass() {
+      // this.main.classList.add('glo-slider');
+      // this.wrap.classList.add('glo-slider__wrap');
+
+      // for(let item of this.slides) {
+      //   item.classList.add('glo-slider__item');
+      // }
+  }
+
+  addSlyles() {
       const style = document.createElement('style');
-      style.id = 'hintsSlider-style';
+      style.id = `${this.wrapSelector}-style`;
       document.head.append(style);
       style.textContent = `
-          .glo-slider{
-              overflow: hidden !important;
-          }
-          .glo-slider__wrap{
+          ${this.wrapSelector}{
               display: flex !important;
               flex-wrap: nowrap !important;
               align-items: center !important;
@@ -52,40 +56,82 @@ class Slider{
               will-change: transform !important;
               margin: 0 auto !important;
           }
-          .glo-slider__item{
+          ${this.slideSelector}{
               flex: 0 0 ${this.options.slideWidth}% !important;
               margin: auto 0 !important;
           }
       `;
+
+      if (!this.reviewsSlider) {
+        style.textContent += `          ${this.mainSelector}{
+          overflow: hidden !important;
+      }`;
+      }
   }
 
-  controlSlider(){
+  controlSlider() {
       this.prev.addEventListener('click', this.prevSlider.bind(this));
       this.next.addEventListener('click', this.nextSlider.bind(this));
   }
 
-  prevSlider(){
+  prevSlider() {
       if(this.options.infinity || this.options.position > 0){
           --this.options.position;
           if(this.options.position < 0){
               this.options.position = this.slides.length - this.slidesToShow;
           }
-          this.wrap.style.transform = `translateX(-${this.options.position * this.options.slideWidth}%)`;
+
+          if (this.reviewsSlider) {
+            for (let slide of this.slides) {
+              slide.style.transform = `translateX(-${this.options.position * this.options.slideWidth}%)`;
+            }
+          } else {
+            this.wrap.style.transform = `translateX(-${this.options.position * this.options.slideWidth}%)`;
+          }
       }
+
+      this.updateArrowVisibility();
       
       this.highlightActiveItem();
   }
 
-  nextSlider(){
+  nextSlider() {
       if(this.options.infinity || this.options.position < this.slides.length - this.slidesToShow){
             ++this.options.position;
             if(this.options.position > this.slides.length - this.slidesToShow){
                 this.options.position = 0;
           }
-          this.wrap.style.transform = `translateX(-${this.options.position * this.options.slideWidth}%)`;  
+
+          
+          if (this.reviewsSlider) {
+            for (let slide of this.slides) {
+              slide.style.transform = `translateX(-${this.options.position * this.options.slideWidth}%)`;
+            }
+          } else {
+            this.wrap.style.transform = `translateX(-${this.options.position * this.options.slideWidth}%)`;
+          }
+          //this.wrap.style.transform = `translateX(-${this.options.position * this.options.slideWidth}%)`;  
              
+          this.updateArrowVisibility();
+
           this.highlightActiveItem();
       }
+  }
+
+  updateArrowVisibility() {
+    if (!this.options.infinity) {
+      if (this.options.position === 0) {
+        this.prev.style.visibility= 'hidden';
+      } else {
+        this.prev.style.visibility= 'visible';
+      }
+
+      if (this.options.position === this.slides.length - this.slidesToShow) {
+          this.next.style.visibility= 'hidden';
+      } else {
+        this.next.style.visibility= 'visible';
+      }
+    }
   }
 
   highlightActiveItem() {
@@ -98,7 +144,7 @@ class Slider{
     }
   }
 
-  addArrow(){
+  addArrow() {
 
   }
 }
@@ -109,18 +155,31 @@ const initSliders = () => {
     wrap: '.formula-slider',
     next: '#formula-arrow_right',
     prev: '#formula-arrow_left',
-    slidesToShow: 3,
+    slideSelector: '.formula-slider__slide',
     activeItemClass: 'active-item'
 });
   formulaSlider.init();
 
   const repairTypesSliders = document.querySelectorAll('[class^="types=repair"]');
 
+  const reviewsSlider = new Slider({
+    main: '.reviews-slider-wrap',
+    wrap: '.reviews-slider',
+    next: '#reviews-arrow_right',
+    prev: '#reviews-arrow_left',
+    slideSelector: '.reviews-slider__slide',
+    reviewsSlider: true,
+    infinity: false
+});
+
+  reviewsSlider.init();
+
   const partnersSlider = new Slider({
     main: '.partners>.wrapper',
     wrap: '.partners-slider',
     next: '#partners-arrow_right',
     prev: '#partners-arrow_left',
+    slideSelector: '.partners-slider__slide',
     slidesToShow: 3
 });
 
