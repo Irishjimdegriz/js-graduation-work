2cbf344d42d23977d350cb220d84231529304abe
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-07-24 21:54:08 +0300

    fix: error fix

--- src/modules/fullList.js
+++ src/modules/fullList.js
@@ -1,7 +1,9 @@
+const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+
 const fullList = () => {
   const setStartingPosition = (elem) => {
     if (elem.style.right != 0) {
-      if (document.documentElement.clientWidth < 576) {
+      if (window.innerWidth < 576) {
         elem.style.transition = '0';
         elem.style.cssText = `top: -${elem.clientHeight}px; right: ${elem.clientWidth}px`;
         elem.style.transition = '1s';
@@ -13,11 +15,10 @@ const fullList = () => {
 
   document.addEventListener('click', (event) => {
     const popupRepairTypes = document.querySelector('.popup-repair-types');
-
     if (event.target.closest('.link-list-menu') || event.target.closest('.link-list-repair')) {
       popupRepairTypes.style.visibility = 'visible';
-      setStartingPosition();
-    } else if (!event.target.closest('.popup-dialog-repair-types')) {
+      setStartingPosition(popupDialogMenu);
+    } else if (!event.target.closest('.popup-dialog-repair-types') || event.target.closest('.close')) {
       popupRepairTypes.style.visibility = 'hidden';
     }
   });
