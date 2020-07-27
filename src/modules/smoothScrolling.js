d9d98e25e81f70a1066e01b9687cb36850a27af4
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-07-27 23:52:51 +0300

    fix: smooth scrolling in IE

--- src/modules/smoothScrolling.js
+++ src/modules/smoothScrolling.js
@@ -1,6 +1,43 @@
+const scrollFunction = (event) => {
+  event.preventDefault();
+
+  if (event.target.tagName.toLowerCase() === "a") {
+    let targetLink = event.target.getAttribute("href").substring(1),
+        token,
+        offsetTop = document.getElementById(targetLink).offsetTop,
+        scrollTop = document.documentElement.scrollTop,
+        bottom = scrollTop < offsetTop;
+
+        token = requestAnimationFrame(function calculate() {
+          scrollTop = bottom ? scrollTop + 100 : scrollTop - 100;
+          document.documentElement.scrollTop = scrollTop;
+
+          if ((scrollTop >= offsetTop && bottom) || (scrollTop < offsetTop && !bottom)) {
+            cancelAnimationFrame(token);
+          } else {
+            calculate();
+          }
+        });
+
+  }
+};
+
 const smoothScrolling = () => {
+  const links = document.querySelector(".popup-dialog-menu"),
+        footerLink = document.querySelector(".button-footer a");
+
+  links.addEventListener("click", () => {
+    if (event.target.closest(".popup-menu-nav__item")) {
+      scrollFunction(event);
+    }
+  });
+
+  footerLink.addEventListener("click", scrollFunction);
+  
   const html = document.querySelector('html');
   html.style.scrollBehavior = 'smooth';
 }
 
+
+//smoothScrolling();
 export default smoothScrolling;
